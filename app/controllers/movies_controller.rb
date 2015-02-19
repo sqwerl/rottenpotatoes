0e5eae507e3a01204ffd11c56c6e105bb9c831dd
Larry Cao <lcao@twitter.com>
2015-02-18 23:58:18 -0800

    finish

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,14 +7,41 @@ class MoviesController < ApplicationController
   end
 
   def index
-    sort = params[:sort]
-    if sort == 'title'
-      @movies = Movie.order('title')
-    elsif sort == 'date'
-      @movies = Movie.order('release_date')
-    else
-      @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = params[:ratings]
+    @find_hash = Hash.new
+
+    session_bool = false
+    @order = nil
+    if (!@selected_ratings)
+      @selected_ratings = Hash.new
     end
+
+    if params.has_key?(:sort)
+      @order = session[:sort] = params[:sort]
+    elsif (session[:sort])
+      session_bool = true
+      @order = session[:sort]
+    end
+
+    if params.has_key?(:ratings)
+      @selected_ratings = session[:selected_ratings] = params[:ratings]
+    elsif (session[:selected_ratings_hash])
+      session_bool = true
+      @selected_ratings = session[:selected_ratings]
+    end
+
+
+    if (session_bool)
+      redirect_to movies_path({:sort=>@order,
+                               :ratings=>@selected_ratings})
+    end
+
+    find = Hash.new
+    find[:conditions] = Hash[:rating => @selected_ratings.keys]
+    find[:order] = @order
+
+    @movies = Movie.find(:all, find)
   end
 
   def new
